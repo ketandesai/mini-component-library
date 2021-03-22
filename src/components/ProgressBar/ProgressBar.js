b5dad39dce1bfba4fb87898935e2f29894de35fe
ketandesai <desai.ketan@gmail.com>
2021-03-22 19:19:37 -0400

    added progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Bar
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax={size}
+    >
+      {value} %
+    </Bar>
+  );
 };
+
+const Bar = styled.div`
+  position: absolute;
+  width: 370px;
+  height: 8px;
+
+  /* transparent-gray-15 */
+
+  background: rgba(128, 128, 128, 0.15);
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+  border-radius: 4px;
+`;
 
 export default ProgressBar;
